55af899319e15d2e45136e65c84cb330bc223738
usuario <usuario@10.188.104.16>
2022-12-16 13:24:13 +0100

    v5.-Para clear esta clase, rodee el método PiFunction y le di a refactor, extraer SuperClass(marqué dentro el método PiFunction), y le di nombre.

--- CalcularPI/src/calcularpi/CalcPI.java
+++ CalcularPI/src/calcularpi/CalcPI.java
@@ -4,7 +4,7 @@ package calcularpi;
 import java.math.BigDecimal;
 import java.math.MathContext;
 
-public class CalcPI {
+public class CalcPI extends ClasePadre {
 
      public static void main(String[] args) {
          final int NUM_DIG = 10; 
@@ -19,8 +19,7 @@ public class CalcPI {
         BigDecimal limit = new BigDecimal(1).movePointLeft(NUM_DIG);
         boolean stop = false;
         for (int k = 0; !stop; k++) {
-            BigDecimal piK = piFunction(k, mc);
-            //meter en el método piFunction                
+            BigDecimal piK = piFunction(k, mc);              
             pi = pi.add(piK);
             if (piK.compareTo(limit) < 0) {
                 stop = true;
@@ -29,28 +28,5 @@ public class CalcPI {
         System.out.println(pi.round(mc));
     }
 
-    public static BigDecimal piFunction(int k, MathContext mc) {
-        //Hice click derecho, refactor, introduce, method y puse el nombre de piFunction y le di a finish.
-        int k8 = 8 * k;
-        BigDecimal val1 = new BigDecimal(4);
-        val1 = val1.divide(new BigDecimal(k8 + 1), mc);
-        BigDecimal val2 = new BigDecimal(-2);
-        val2 = val2.divide(new BigDecimal(k8 + 4), mc);
-        BigDecimal val3 = new BigDecimal(-1);
-        val3 = val3.divide(new BigDecimal(k8 + 5), mc);
-        BigDecimal val4 = new BigDecimal(-1);
-        val4 = val4.divide(new BigDecimal(k8 + 6), mc);
-        BigDecimal val = val1;
-        val = val.add(val2);
-        val = val.add(val3);
-        val = val.add(val4);
-        BigDecimal multiplier = new BigDecimal(16);
-        multiplier = multiplier.pow(k);
-        BigDecimal one = new BigDecimal(1);
-        multiplier = one.divide(multiplier, mc);
-        val = val.multiply(multiplier);
-        BigDecimal piK = val;
-        return piK;
-    }
      
 }
