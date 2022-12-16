8eff9aa36632b04b8de52153d28492d027a8d155
usuario <usuario@10.188.104.16>
2022-12-16 13:11:15 +0100

    v3.-cree una constante llamada NUM_DIG que reemplazo todos los número 10 que hubiera en el código

--- CalcularPI/src/calcularpi/CalcPI.java
+++ CalcularPI/src/calcularpi/CalcPI.java
@@ -7,10 +7,12 @@ import java.math.MathContext;
 public class CalcPI {
 
      public static void main(String[] args) {
-        MathContext mc = new MathContext(10);         
+         final int NUM_DIG = 10; 
+         //cree una constante llamada NUM_DIG que reemplazo todos los número 10 que hubiera en el código 
+        MathContext mc = new MathContext(NUM_DIG);         
         //meter en el método compute
         BigDecimal pi = new BigDecimal(0);
-        BigDecimal limit = new BigDecimal(1).movePointLeft(10);
+        BigDecimal limit = new BigDecimal(1).movePointLeft(NUM_DIG);
         boolean stop = false;
         for (int k = 0; !stop; k++) {
             BigDecimal piK = piFunction(k, mc);
